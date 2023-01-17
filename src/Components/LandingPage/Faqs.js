9e72b1f9f89e6e798463bda4b753a6198e048eed
Ashish Verma <113393491+the3rdeyecan@users.noreply.github.com>
2023-01-17 08:54:15 -0700

    git init

---
+++ src/Components/LandingPage/Faqs.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Faqs.css';
+
+const Faqs = () => {
+  return (
+    <div className='faqs'>
+      <h2>Frequently Asked Questions</h2>
+
+      <div className='faq-container'>
+        <div className='each-faq'>
+          <h3>WHO ARE WE?</h3>
+          <p>
+            Africa Live Network TV is a media company that is focused on
+            providing quality content to Africans and the world at large. We are
+            a team of young, vibrant, and creative individuals who are
+            passionate about telling stories that matter. We are a team of
+            young, vibrant, and creative individuals who are passionate about
+            telling stories that matter.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,58 @@
+import ALNLogo from '../../assets/ALN_LOGO-3-47.png';
+import { SiFacebook } from 'react-icons/si';
+import { SiInstagram } from 'react-icons/si';
+import { SiTwitter } from 'react-icons/si';
+import { IconContext } from 'react-icons';
+import Form from './WaitlistForm';
+import './LandingPage.css';
+import Faqs from './Faqs';
+
+function LandingPage() {
+  return (
+    <div className='landing-page'>
+      <header>
+        {/* <img src = {} alt="" /> */}
+        <img src={ALNLogo} alt='Finall Logo' />
+        <h1> We are coming soon... </h1>
+        <p>
+          We are almost there! If you want to get notified when the website goes
+          live, subscribe to our mailing list and kindly follow us on all our
+          social media handles.
+        </p>
+      </header>
+
+      <Form />
+
+      <Faqs />
+
+      <footer>
+        <IconContext.Provider value={{ className: 'bottom-react-icons' }}>
+          <a
+            href='https://www.facebook.com/africalivenetwork'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <SiFacebook color='#0080ff' />
+          </a>
+
+          <a
+            href='https://twitter.com/Africalivenetw1'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <SiTwitter color='#0080ff' />
+          </a>
+          <a
+            href='https://www.instagram.com/africalivenetwork.tv/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <SiInstagram color='#0080ff' />
+          </a>
+        </IconContext.Provider>
+      </footer>
+    </div>
+  );
+}
+
+export default LandingPage;
